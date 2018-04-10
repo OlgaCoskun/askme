baffa0d1b747375d1a6a5a9c2dc5058eea8ade54
Olga Coskun <ovin999@gmail.com>
2018-04-10 12:55:54 +0300

    add validates

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,33 +1,35 @@
 class Question < ApplicationRecord
 
-  validates :text, presence: true
+  belongs_to :user
 
-  # демонстрация жизненного цикла объекта
-  # навесили на все популярные коллбэки свои методы
-  before_validation :before_validation
-  after_validation :after_validation
-  before_save :before_safe
-  before_create :before_create
-  after_create :after_create
-  after_save :after_safe
+  validates :text, :user, presence: true
 
-  before_update :before_update
-  after_update :after_update
-
-  before_destroy :before_destroy
-  after_destroy :after_destroy
-
-  private
-
-  # динамически сгенерируем пару методов для каждого действия,
-  # используя возможности руби
-  ['validatioin', 'save', 'create', 'update', 'destroy'].each do |action|
-    ['before', 'after'].each do |time|
-      define_method("#{time}_#{action}") do
-        puts "**********> #{time} #{action}"
-      end
-
-    end
-  end
+  # # демонстрация жизненного цикла объекта
+  # # навесили на все популярные коллбэки свои методы
+  # before_validation :before_validation
+  # after_validation :after_validation
+  # before_save :before_save
+  # before_create :before_create
+  # after_create :after_create
+  # after_save :after_save
+  #
+  # before_update :before_update
+  # after_update :after_update
+  #
+  # before_destroy :before_destroy
+  # after_destroy :after_destroy
+  #
+  # private
+  #
+  # # динамически сгенерируем пару методов для каждого действия,
+  # # используя возможности руби
+  # ['validation', 'save', 'create', 'update', 'destroy'].each do |action|
+  #   ['before', 'after'].each do |time|
+  #     define_method("#{time}_#{action}") do
+  #       puts "**********> #{time} #{action}"
+  #     end
+  #
+  #   end
+  # end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,49 @@
+require 'openssl'
+
 class User < ApplicationRecord
+  # параметры работы модуля шифрования паролей
+  ITERATIONS = 20000
+  DIGEST = OpenSSL::Digest::SHA256.new
+
+  has_many :questions
 
   validates :email, :username, presence: true
   validates :email, :username, uniqueness: true
+
+  attr_accessor :password
+
+  validates_presence_of :password, on: :create
+  validates_confirmation_of :password
+
+  before_save :encrypt_password
+
+  def encrypt_password
+    if self.password.present?
+      # создаем т.н. "соль" - рандомная строка усложняющая задачу хакерам
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      # создаем хеш пароля - длинная уникальная строка, из которой невозможно восстановить
+      # исходный пароль
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+      )
+    end
+  end
+
+  # служебный метод, преобразующий бинарную строку в 16-ричный формат, для удобства хранения
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  def self.authenticate(email, password)
+    user = find_by(email: email) # сперва находим кандидатов по email
+
+    # ОБРАТИТЕ внимание: сравнивается password_hash, а оригинальный пароль так никогда
+    # и не сохраняется нигде!
+    if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
+      user
+    else
+      nil
+    end
+  end
 end
