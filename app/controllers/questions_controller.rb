0ef964ac6dc8a57ca12dbe43ffbf07c055c70c59
Olga Coskun <ovin999@gmail.com>
2018-04-25 17:03:07 +0300

    Fix method destroy for user

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,7 +10,7 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
 
-    @question.author = current_user
+    @question[:author_id] = current_user.id if current_user.present?
 
     if @question.save
       redirect_to user_path(@question.user), notice: 'Вопрос задан'
