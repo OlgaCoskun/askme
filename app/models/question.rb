75383cc7a6f2d0ef9eda943244af33afe80fa846
Olga Coskun <ovin999@gmail.com>
2018-07-02 15:41:21 +0300

    Fix

--- app/models/question.rb
+++ app/models/question.rb
@@ -13,7 +13,7 @@ class Question < ApplicationRecord
       self.tags.destroy_all
 
       (text + ' ' + answer.to_s).scan(/#[[:word:]]+/).uniq.each do |hashtag|
-        self.tags << Tag.first_or_create_by!(name: hashtag.delete('#'))
+        self.tags << Tag.first_or_create!(name: hashtag.delete('#'))
       end
     end
   end
